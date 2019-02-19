2de47ccd1de253a66af0c62cb938d2fbebd2f62c
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-02-19 17:59:10 -0300

    conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,7 @@ class Counter extends Component {
 	state = {
 		count: 1,
 		imageUrl: 'https://picsum.photos/g/200/300',
-		tags: [ 'tag1', 'tag2', 'tag3' ],
+		tags: [ 'Allan', 'Ronaldo', 'Pelé' ],
 		address: {
 			street: 'Av Paulista'
 		}
@@ -13,20 +13,33 @@ class Counter extends Component {
 	render() {
 		return (
 			<div>
-				<span className={this.setColorBadge()}>{this.formatCount()}</span>
+				{/* <span className={this.setColorBadge()}>{this.formatCount()}</span> */}
 				{/* <div class="alert alert-dark" role="alert">
                     {this.state.address.street}
                 </div> */}
-				<button className="btn btn-secondary btn-sm">Increment</button>
-				<ul>
-					{this.state.tags.map((tag, index) => (
-						<li id={index} key={tag.toString()}>
-							{tag}
-						</li>
-					))}
-				</ul>
+				{/* <button className="btn btn-secondary btn-sm">Increment</button> */}
+				{this.state.tags.length === 0 && <p className="alert alert-primary">Vazio</p>}
+				{this.renderTags()}
 			</div>
 		);
+	}
+
+	showMenu() {
+		return (
+			<ul className="list-group">
+				{this.state.tags.map((tag, index) => (
+					<li className="list-group-item" id={index} key={tag}>
+						{tag}
+					</li>
+				))}
+			</ul>
+		);
+	}
+
+	renderTags() {
+		if (this.state.tags.length === 0) return <p className="alert alert-danger">There are no tags!</p>;
+
+		return this.showMenu();
 	}
 
 	setColorBadge() {
