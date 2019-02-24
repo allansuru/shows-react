d89b422e0e9635103c163461be8753a03fc4406d
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-02-24 16:42:54 -0300

    lifecycle part-

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -29,6 +29,18 @@ class Counter extends Component {
 	// 	});
 	// };
 
+	componentDidUpdate(prevProps, prevState) {
+		console.log('prevProps: ', prevProps);
+		console.log('prevState: ', prevState);
+		if (prevProps.counter.value !== this.props.counter.value) {
+			// Ajax call and get new data from the server
+		}
+	}
+
+	componentWillUnmount() {
+		console.log('Counter - Unmount');
+	}
+
 	onDelete = () => {
 		console.log('on Deleteee', this.props.onDelete);
 	};
